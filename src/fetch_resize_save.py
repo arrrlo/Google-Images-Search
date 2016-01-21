e0eb2e50b89eadd9d8287c0fff29b9341ef9a90e
Ivan Arar <ivan.arar@gmail.com>
2016-01-21 21:43:13 +0100

    first commit

---
+++ src/fetch_resize_save.py
@@ -0,0 +1,46 @@
+
+__author__ = 'ivan.arar@gmail.com'
+
+
+import os
+import shutil
+import requests
+from PIL import Image
+from resizeimage import resizeimage
+
+import settings
+from google_api import GoogleCustomSearch
+
+
+class FetchResizeSave:
+
+    def __init__(self, search_params, path_to_image):
+        self.search_params = search_params
+        self.path_to_image = path_to_image
+
+    def fetch_image_from_google(self):
+        return GoogleCustomSearch().photo_from_google_image(**self.search_params)
+
+    def run(self):
+        image_from_google, google_exception = self.fetch_image_from_google()
+
+        if not image_from_google:
+            return False, google_exception
+        else:
+            dir_to_image = '/'.join(self.path_to_image.split('/')[:-1])
+            if not os.path.exists(dir_to_image):
+                os.makedirs(dir_to_image)
+
+            req = requests.get(image_from_google, stream=True)
+            if req.status_code == 200:
+                with open(self.path_to_image, 'wb') as file_on_disk:
+                    req.raw.decode_content = True
+                    shutil.copyfileobj(req.raw, file_on_disk)
+
+            fd_img = open(self.path_to_image, 'r')
+            img = Image.open(fd_img)
+            img = resizeimage.resize_cover(img, settings.IMAGE_SIZE)
+            img.save(self.path_to_image, img.format)
+            fd_img.close()
+
+            return True, None
---
+++ src/run.py
@@ -0,0 +1,28 @@
+
+__author__ = 'ivan.arar@gmail.com'
+
+
+import settings
+from fetch_resize_save import FetchResizeSave
+
+if __name__ == '__main__':
+
+    search_params = {
+        'q': '__my_search_query__',
+        'num': 5,
+        'safe': 'off',
+        'fileType': 'jpg',
+        'imgType': 'photo',
+        'imgSize': 'large',
+        'searchType': 'image',
+        'imgDominantColor': 'black' 
+    }
+    path_to_image = settings.IMAGE_PATH[0] % '__my_image__.jpg'
+    
+    success, fail = FetchResizeSave(search_params=search_params, path_to_image=path_to_image).run()
+
+    if success:
+        # Do something with this new resized and locally saved image
+        print(path_to_image)
+    else:
+        print(fail)
